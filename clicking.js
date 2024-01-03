6e1d0df74762111e443a9fc6ab71f14e01bfb7aa
Miniek <69723765+Miniek1@users.noreply.github.com>
2024-01-03 22:31:08 +0100

    The showing thing is working now.

--- clicking.js
+++ clicking.js
@@ -1,8 +1,19 @@
-var clicks = 0
-const start = 50
-
+var clicks = 0;
+const start = 50;
 
 function clicking() {
-    clicks++
-    document.getElementById('clicks').innerHTML = clicks
-}
+    clicks++;
+    document.getElementById('clicks').innerHTML = clicks;
+
+    if (clicks === start) {
+        ShowTheGoods();
+        console.log("Showing the UI...")
+    };
+};
+
+function ShowTheGoods() {
+    document.getElementById('shop').style.transform = "translate(0, 0)";
+    document.getElementById('hamburger').style.transform = "translate(0, 0)";
+    document.getElementById('checkbox1').style.transform = "translate(0, 0)";
+    document.getElementById('checkbox2').style.transform = "translate(0, 0)";
+};
