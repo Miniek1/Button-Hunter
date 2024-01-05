136ba5fae3a69fbc4c41a179ab5d879b1ff7a81f
Miniek <69723765+Miniek1@users.noreply.github.com>
2024-01-05 01:31:32 +0100

    added the shop-menu window

--- clicking.js
+++ clicking.js
@@ -1,14 +1,16 @@
 var clicks = 0;
 var totalclicks = 0;
 var ui = "false";
+var shop = false;
 
+// Don't touch this, it somehow works
 function setCookie(cname,cvalue,exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-
+// Don't touch this, it somehow works
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -31,7 +33,7 @@ var clicks = getCookie("clicks")
 if (ui === "true") {
     ShowTheGoods();
     console.log("Showing the UI...");
-};
+}
 
 if (clicks != 0) { 
     document.getElementById('clicks').innerHTML = clicks;
@@ -42,23 +44,38 @@ function clicking() {
     document.getElementById('clicks').innerHTML = clicks;
 
     setCookie("clicks", clicks, 999999)
+    setCookie("totalclicks", totalclicks, 999999)
 
     if (totalclicks === 50) {
         setCookie("ui", true, 999999)
         ShowTheGoods();
         console.log("Showing the UI...");
-    };
-};
+    }
+}
 
 function ShowTheGoods() {
     document.getElementById('shop-button').style.transform = "translate(0, 0)";
     document.getElementById('hamburger-button').style.transform = "translate(0, 0)";
     document.getElementById('checkbox1').style.transform = "translate(0, 0)";
     document.getElementById('checkbox2').style.transform = "translate(0, 0)";
-};
+}
 
-function Hamburger() {
+function HamburgerMenu() {
 
 }
 
-document.getElementById("button").addEventListener('click', clicking)
+function ShopMenu() {
+    if (shop === false) {
+        document.getElementById('shop-menu').style.transform = "translate(0, 0)";
+        document.getElementById('shop-menu').style.transition = "transition: transform 0.3s ease-in;";
+        console.log("Showing the shop");
+        shop = true;
+    } else if (shop) {
+        document.getElementById('shop-menu').style.transform = "translate(100%, 0)";
+        document.getElementById('shop-menu').style.transition = "transition: transform 0.3s ease-out;";
+        shop = false;
+    }
+}
+
+document.getElementById("button").addEventListener('click', clicking);
+document.getElementById("shop-button").addEventListener('click', ShopMenu);
