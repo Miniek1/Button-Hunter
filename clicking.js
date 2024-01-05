ebf79919e4dd39092c7ec0d743c5d7ff0af33e93
Miniek <69723765+Miniek1@users.noreply.github.com>
2024-01-05 23:34:27 +0100

    changed the numbers so theyre actually readable now

--- clicking.js
+++ clicking.js
@@ -45,18 +45,54 @@ if (ui === "true") {
 }
 
 if (clicks != "0") { 
-    document.getElementById('clicks').innerHTML = parseInt(clicks);
+    document.getElementById('clicks').innerHTML = abbrNum(parseInt(clicks), 1);
 }
 
 if (OneClickPrice != 50) {
-    document.getElementById('buy-button').innerHTML = parseInt(OneClickPrice);
+    document.getElementById('buy-button').innerHTML = abbrNum(parseInt(OneClickPrice), 1);
 }
 
+// Absolutely stole this function from stackoverflow but i dont care anymore
+function abbrNum(number, decPlaces) {
+    // 2 decimal places => 100, 3 => 1000, etc
+    decPlaces = Math.pow(10, decPlaces);
+
+    // Enumerate number abbreviations
+    var abbrev = ["k", "m", "b", "t", "q", "Q", "s", "S", "o", "n", "d", "u", "D", "T"];
+
+    // Go through the array backwards, so we do the largest first
+    for (var i = abbrev.length - 1; i >= 0; i--) {
+
+    // Convert array index to "1000", "1000000", etc
+    var size = Math.pow(10, (i + 1) * 3);
+
+      // If the number is bigger or equal do the abbreviation
+    if (size <= number) {
+    // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+    // This gives us nice rounding to a particular decimal place.
+    number = Math.round(number * decPlaces / size) / decPlaces;
+
+    // Handle special case where we round up to the next abbreviation
+    if ((number == 1000) && (i < abbrev.length - 1)) {
+        number = 1;
+        i++;
+    }
+
+        // Add the letter for the abbreviation
+        number += abbrev[i];
+
+        // We are done... stop
+        break;
+        }
+    }
+
+    return number;
+}
 
 function clicking() {
     clicks = parseInt(clicks) + parseInt(clickperclick);
     totalclicks++;
-    document.getElementById('clicks').innerHTML = clicks;
+    document.getElementById('clicks').innerHTML = abbrNum(clicks, 1);
 
     setCookie("clicks", clicks, 999999)
     setCookie("totalclicks", totalclicks, 999999)
@@ -114,10 +150,10 @@ function OneClickUpgrade() {
         setCookie("clicks", clicks, 999999)
 
         clicks = clicks - OneClickPrice;
-        document.getElementById('clicks').innerHTML = parseInt(clicks);
+        document.getElementById('clicks').innerHTML = abbrNum(parseInt(clicks), 1);
         OneClickPrice = OneClickPrice * 1.5;
         setCookie("OneClickPrice", parseInt(OneClickPrice), 999999)
-        document.getElementById('buy-button').innerHTML = parseInt(OneClickPrice);
+        document.getElementById('buy-button').innerHTML = abbrNum(parseInt(OneClickPrice), 1);
     }
 }
 
